27ddf5f2a2d09a8018f6d066a4801392b2b05361
Ramon Ribeiro <ramon.procisa@gmail.com>
2023-07-28 15:31:11 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/ramonribeiro/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.ramonribeiro.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramonribeiro.workshopmongo.domain.User;
+import com.ramonribeiro.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+		
+	}
+
+}
