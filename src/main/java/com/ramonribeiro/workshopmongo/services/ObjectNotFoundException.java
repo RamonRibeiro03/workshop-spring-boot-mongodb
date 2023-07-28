594d53af5e8f0a287f0338316630f8958de0571f
Ramon Ribeiro <ramon.procisa@gmail.com>
2023-07-28 17:28:56 -0300

    Obtendo um usuario por ID

---
+++ src/main/java/com/ramonribeiro/workshopmongo/services/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.ramonribeiro.workshopmongo.services;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException (String msg) {
+		super(msg);
+	}
+	
+	
+	
+	
+
+}
