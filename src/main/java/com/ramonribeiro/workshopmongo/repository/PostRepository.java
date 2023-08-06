d542ed4a7e38febb0338e2201a79420ba93c4969
Ramon Ribeiro <ramon.procisa@gmail.com>
2023-08-06 00:13:27 -0300

    Consulta simples com @Query

--- src/main/java/com/ramonribeiro/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ramonribeiro/workshopmongo/repository/PostRepository.java
@@ -1,13 +1,18 @@
 package com.ramonribeiro.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ramonribeiro.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	@Query("{ 'title': { $regex: ?0, 'i' }}")
+	List<Post> searchTitle(String text);
 
 
 }
--- src/main/java/com/ramonribeiro/workshopmongo/services/PostService.java
+++ src/main/java/com/ramonribeiro/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.ramonribeiro.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,8 @@ public class PostService {
 	public Post findById(String id) {
 	    Optional<Post> user = repo.findById(id);
 	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	public List<Post> findByTitle(String text){
+		return repo.searchTitle(text);
 	}
 }
